e39ffde187439a7e20457280b8c2820f81ebbd5c
Emma-Marie <emma-marie@outlook.dk>
2023-05-07 17:38:27 +0200

    working 7th

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # assignment4_self-assigned_Emma-Marie
+
+## Description and purpose
+Image search with VGG16 and K-Nearest Neighbours.
+
+## Data
+I have been using a dataset called "Cultural Heritage Dataset - Orthodox Churches" from Kaggle. The data set contains 800 images divided into four classes with 200 images in each class. Each image is 128x128 pixels. 
---
+++ utils/features.py
@@ -0,0 +1,22 @@
+def extract_features(img_path, model):
+    """
+    Extract features from image data using pretrained model (e.g. VGG16)
+    """
+    # Define input image shape - remember we need to reshape
+    input_shape = (224, 224, 3) # the shape is set to match the images in the "Cultural Heritage Dataset"
+    # load image from file path
+    img = load_img(img_path, target_size=(input_shape[0], 
+                                          input_shape[1]))
+    # convert to array
+    img_array = img_to_array(img)
+    # expand to fit dimensions
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    # preprocess image - see last week's notebook
+    preprocessed_img = preprocess_input(expanded_img_array)
+    # use the predict function to create feature representation
+    features = model.predict(preprocessed_img)
+    # flatten
+    flattened_features = features.flatten()
+    # normalise features
+    normalized_features = flattened_features / norm(features) # norm is from numpy --> a slightly more sophisticated type of normalization than just deviding by 255 (but the latter could also be done). 
+    return normalized_features
